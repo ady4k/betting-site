027e7a6b4e5c459f06c514b74b7a1779402f20da
ady4k <ady4k@users.noreply.github.com>
2022-01-23 23:10:12 +0200

    initial commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int Password { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+        public int Money { get; set; }
+
+    }
+}
